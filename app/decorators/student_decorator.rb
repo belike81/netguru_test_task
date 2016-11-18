315a61fbfe29ed51d969c2f4046ca144a7097d84
Tomasz Grabowski <belike81@gmail.com>
2016-11-18 11:28:58 +0100

    Add missing avg_notes method body

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,8 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item.subject_item_notes.pluck(:value)
+    notes = [0] if notes.empty?
+    h.number_with_precision(notes.inject(:+).to_f / notes.size, precision: 2)
   end
 end
