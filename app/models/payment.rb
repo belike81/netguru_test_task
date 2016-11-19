e0221d49c3db0c29b572c11a8b27b03862a53528
Tomasz Grabowski <belike81@gmail.com>
2016-11-19 17:11:25 +0100

    Add first validation and specs to payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,5 @@
 class Payment < ActiveRecord::Base
   belongs_to :student
+
+  validates :student_id, :amount, :completion_date, presence: true
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :student_id }
+    it { is_expected.to validate_presence_of :amount }
+    it { is_expected.to validate_presence_of :completion_date }
+  end
+
+  describe 'database columns' do
+    it { should have_db_column :student_id }
+    it { should have_db_column :amount }
+    it { should have_db_column :completion_date }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to :student }
+  end
 end
