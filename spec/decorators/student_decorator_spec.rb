a5f73145fc1ea574bdd5d5b919986418406b5351
Tomasz Grabowski <belike81@gmail.com>
2016-11-19 12:36:00 +0100

    Add spec for decorator birthday method

--- spec/decorators/student_decorator_spec.rb
+++ spec/decorators/student_decorator_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe StudentDecorator do
   let(:teacher) { create :teacher, first_name: 'John', last_name: 'Smith' }
-  let(:student) { create :student, first_name: 'John', last_name: 'Smith' }
+  let(:student) { create :student, first_name: 'John', last_name: 'Smith', birthdate: '1980-01-01' }
   let(:subject_item) { create :subject_item, teacher: teacher }
   let(:second_subject_item) { create :subject_item }
   let!(:note_1) { create :subject_item_note, value: 5, student: student, subject_item: second_subject_item }
@@ -30,4 +30,18 @@ describe StudentDecorator do
       end
     end
   end
+
+  describe "#birthday" do
+    context 'birthdate is set on student' do
+      subject { student.decorate.birthday }
+      it { is_expected.to eq '1980-01_01' }
+    end
+
+    context 'birthdate is not set on student' do
+      before { student.birthdate = nil }
+      subject { student.decorate.birthday }
+      it { is_expected.to eq I18n.t('shared.not_provided') }
+    end
+  end
+
 end
