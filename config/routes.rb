c28f77864980b998bba53536b0b4db22a37f2f1a
Tomasz Grabowski <belike81@gmail.com>
2016-11-22 11:02:44 +0100

    Add payment routes and navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'payments/index'
+
   root to: 'visitors#index'
 
   devise_for :users
@@ -9,6 +11,8 @@ Rails.application.routes.draw do
   resources :teachers do
     get :subjects
   end
+
+  resources :payments
 
   get 'reports/subjects', to: 'reports#subjects', as: :report_subjects
   get 'visitors', to: 'visitors#index'
